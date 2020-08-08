f5f892589123eedbbc09862b57ba4ee95ee9b14f
vikramray <vikram.ray@ezedox.com>
2020-08-09 00:44:12 +0530

    Updates

---
+++ precache-manifest.2bad999aea22b7f046d96a42381fef90.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48347c302507ca82244d9bf06c309e76",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a160bd91cb09363a7149",
+    "url": "/static/css/3.f9898e9a.chunk.css"
+  },
+  {
+    "revision": "37f257b11df7a63ab5ea",
+    "url": "/static/css/4.54df59d4.chunk.css"
+  },
+  {
+    "revision": "b02cb22584ff226229bc",
+    "url": "/static/css/main.f0ed6029.chunk.css"
+  },
+  {
+    "revision": "0a71e9c4c52899eb2a3f",
+    "url": "/static/js/2.156bb72a.chunk.js"
+  },
+  {
+    "revision": "a160bd91cb09363a7149",
+    "url": "/static/js/3.60c11a40.chunk.js"
+  },
+  {
+    "revision": "37f257b11df7a63ab5ea",
+    "url": "/static/js/4.a890e768.chunk.js"
+  },
+  {
+    "revision": "b02cb22584ff226229bc",
+    "url": "/static/js/main.d9fe1e3a.chunk.js"
+  },
+  {
+    "revision": "5380bee336244c0bb4e2",
+    "url": "/static/js/runtime-main.6d2f0992.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  }
+]);
