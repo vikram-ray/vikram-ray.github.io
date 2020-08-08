a404878c6c9f22ba39a620485308901e3f24f850
vikramray <vikram.ray@ezedox.com>
2020-08-09 00:35:53 +0530

    Updates

---
+++ precache-manifest.fcac2991f34de7bea0ed63b1c1fea183.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "279800eeede093c87499441c2c0d79be",
+    "url": "rayvikram.github.io/index.html"
+  },
+  {
+    "revision": "a160bd91cb09363a7149",
+    "url": "rayvikram.github.io/static/css/3.f9898e9a.chunk.css"
+  },
+  {
+    "revision": "37f257b11df7a63ab5ea",
+    "url": "rayvikram.github.io/static/css/4.54df59d4.chunk.css"
+  },
+  {
+    "revision": "b78eac153e505b70e917",
+    "url": "rayvikram.github.io/static/css/main.87ab91d7.chunk.css"
+  },
+  {
+    "revision": "0a71e9c4c52899eb2a3f",
+    "url": "rayvikram.github.io/static/js/2.156bb72a.chunk.js"
+  },
+  {
+    "revision": "a160bd91cb09363a7149",
+    "url": "rayvikram.github.io/static/js/3.60c11a40.chunk.js"
+  },
+  {
+    "revision": "37f257b11df7a63ab5ea",
+    "url": "rayvikram.github.io/static/js/4.a890e768.chunk.js"
+  },
+  {
+    "revision": "b78eac153e505b70e917",
+    "url": "rayvikram.github.io/static/js/main.78788567.chunk.js"
+  },
+  {
+    "revision": "41c1de6ebf7449ab9eff",
+    "url": "rayvikram.github.io/static/js/runtime-main.90a3bfc2.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "rayvikram.github.io/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "rayvikram.github.io/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "rayvikram.github.io/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "rayvikram.github.io/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "rayvikram.github.io/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "rayvikram.github.io/static/media/skill.34409398.svg"
+  }
+]);
