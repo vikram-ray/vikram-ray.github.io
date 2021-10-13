24c5c3d84741a7222b0701cf768757ccd2e2c4a9
rayvikram <vik8876@gmail.com>
2021-10-13 19:03:50 +0530

    added email

--- src/portfolio.js
+++ src/portfolio.js
@@ -310,7 +310,7 @@ const contactInfo = {
   title: emoji("Contact Me ☎️"),
   subtitle: "Discuss a project or just want to say hi? My Inbox is open for all.",
   number: "+91-7974550078",
-  email_address: "vik8876@gmail.com"
+  email_address: "thevikramray@gmail.com"
 };
 
 
